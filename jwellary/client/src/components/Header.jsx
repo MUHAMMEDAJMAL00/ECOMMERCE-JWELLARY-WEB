66675df03698e3c625a6d3441bce2dc903061984
ajmal <wayandajmal@gmail.com>
2025-08-08 15:16:33 +0530

    updated logo

--- jwellary/client/src/components/Header.jsx
+++ jwellary/client/src/components/Header.jsx
@@ -14,7 +14,7 @@ import { clearCart } from "../redux/slices/cartSlice";
 import { clearWishlist } from "../redux/slices/Wishlist";
 
 // Icons
-import goldoralogo from "../assets/images/goldora.png";
+import lucky from "../assets/images/lucky.png";
 import { HiOutlineUser } from "react-icons/hi2";
 import { IoIosSearch } from "react-icons/io";
 import { BsBag } from "react-icons/bs";
@@ -267,7 +267,7 @@ const Header = () => {
               style={{ cursor: "pointer" }}
             />
             <img
-              src={goldoralogo}
+              src={lucky}
               className="header-desklogos"
               onClick={goToHome}
               alt="logo"
