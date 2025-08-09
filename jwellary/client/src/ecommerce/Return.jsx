407f31693f635c1431a1bd9b28eccdaf8856c142
ajmal <wayandajmal@gmail.com>
2025-08-09 14:56:24 +0530

    updated return and myorders

--- jwellary/client/src/ecommerce/Return.jsx
+++ jwellary/client/src/ecommerce/Return.jsx
@@ -16,7 +16,7 @@ const Return = () => {
 
     console.log("📦 Sending return request:", {
       orderId,
-      userId,
+      userId: user?._id,
       productId: item?._id,
       reason,
     });
