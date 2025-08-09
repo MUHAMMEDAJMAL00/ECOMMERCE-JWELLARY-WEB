fd32c5e624a678fafc8cd989cc03cd98babf3a5a
ajmal <wayandajmal@gmail.com>
2025-08-09 14:22:18 +0530

    updated return and myorders

--- server/models/Return.js
+++ server/models/Return.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const returnSchema = new mongoose.Schema({
   orderId: { type: String, required: true }, // Reference to the order
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "user", required: true },
   reason: { type: String, required: true },
   status: { type: String, default: "Pending" }, // Pending, Approved, Rejected
   createdAt: { type: Date, default: Date.now },
